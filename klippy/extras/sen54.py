73e955ac0b019b7eb047a805adf2984bca12122f
John Caspers <john.a.caspers@gmail.com>
2022-06-12 16:01:42 -0500

    adding registers

--- klippy/extras/sen54.py
+++ klippy/extras/sen54.py
@@ -8,11 +8,21 @@ from . import bus
 
 SEN54_CHIP_ADDR = 0x76
 
+SEN54_REGS = {'START_M': 0x0021, 'START_M_RHT': 0x0037}
+
+# Need to determine registers that are being used 
+# Need to be able to read from those registers
+# Need to handle those registers in a way that makes sense
+# Create proper logic to parse data
+# Determine any other constants
+
 class SEN54:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.chip_type = 'SEN54'
         self.chip_address = SEN54_CHIP_ADDR
+        self.i2c = bus.MCU_I2C_from_config(
+            config, default_addr=SEN54_CHIP_ADDR, default_speed=100000)
 
 
 def load_config(config):
