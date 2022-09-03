348c33c50901a5773c29ce7ab3ca3714c6327310
Jack Caspers <john.a.caspers@gmail.com>
2022-09-02 21:40:00 -0500

    adding regs

--- klippy/extras/sen54.py
+++ klippy/extras/sen54.py
@@ -8,7 +8,27 @@ from . import bus
 
 SEN54_CHIP_ADDR = 0x76
 
-SEN54_REGS = {'START_M': 0x0021, 'START_M_RHT': 0x0037}
+SEN54_REGS = {
+    'START_M': 0x0021, 
+    'START_M_RHT': 0x0037,
+    'STOP_M': 0x0104,
+    'READ_DATA_FlAG': 0x0202,
+    'READ_M_VALUES': 0x03C4,
+    'RW_TEMP_COMP_PARAM': 0x60B2,
+    'RW_WARM_START_PARAM': 0x60C6,
+    'RW_VOC_ALGO_TUNE': 0x60D0,
+    'RW_NOX_ALGO_TUNE': 0x60E1,
+    'RW_RHT_ACC_MODE': 0x60F7,
+    'RW_VOC_ALGO_STATE': 0x6181,
+    'START_FAN_CLEANING': 0x5607,
+    'RW_AUTO_CLEANING_INTERVAL': 0x8004,
+    'READ_PRODUCT_NAME': 0xD014,
+    'READ_SERIAL_NUMBER': 0xD033,
+    'READ_FIRM_VERSION': 0xD100,
+    'READ_DEVICE_STATUS': 0xD206,
+    'CLEAR_DEVICE_STATUS': 0xD210,
+    'RESET': 0xD304
+}
 
 # Need to determine registers that are being used 
 # Need to be able to read from those registers
